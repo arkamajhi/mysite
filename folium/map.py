06ddfd7200a47c47bebb728ee97c37a087436209
Arka Majhi <arka.majhi@gmail.com>
2021-04-15 09:49:55 +0530

    Update map.py

--- folium/map.py
+++ folium/map.py
@@ -9,7 +9,12 @@ tooltip='Click for more'
 #Create markers
 folium.Marker([42.363600,-71.099500],
               popup='<strong>Location 1</strong>',
-              tooltip=tooltip).add_to(m)
+              tooltip=tooltip).add_to(m),
+
+folium.Marker([42.333600,-71.099500],
+              popup='<strong>Location 2</strong>',
+              tooltip=tooltip,
+              icon=folium.Icon(icon='cloud')).add_to(m),
 
 #Generate map
 m.save('map.html')
