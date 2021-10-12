e8736abc1cfe48810cdb84f585ed28fd145e7122
Arka Majhi <arka.majhi@gmail.com>
2021-10-13 01:49:50 +0530

    Update js.js

--- wipro_technical/2/js.js
+++ wipro_technical/2/js.js
@@ -91,7 +91,7 @@ $(document).ready(function()
   {
     document.getElementById("container").innerHTML +='<div class="cards '+employee[i].role+'">'
       +'<div class="ImageBox">'
-        +'<img src="images/Images ('+(i+1)+').jpg" alt="images/Images ('+(i+1)+').JPG">'
+        +'<img src="images/Images ('+(i+1)+').jpg" alt="">'
       +'</div>'
       +'<div class="content">'
         +'<div class="details">'
