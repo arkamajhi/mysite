e3522ea1c43ebf8c4c1a6944e30296505a2f06bb
Arka Majhi <arka.majhi@gmail.com>
2021-10-18 01:12:11 +0530

    Create dnd.js

---
+++ draganddrop8/dnd.js
@@ -0,0 +1,71 @@
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  var dragSrcEl = null;
+
+  function handleDragStart(e) {
+    console.log("DragStart");
+    this.style.opacity = '0.4';
+
+    dragSrcEl = this;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+  }
+
+  function handleDragOver(e) {
+    console.log("DragOver");
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+
+    e.dataTransfer.dropEffect = 'move';
+
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    console.log("DragEnter");
+    this.classList.add('over');
+  }
+
+  function handleDragLeave(e) {
+    console.log("DragLeave");
+    this.classList.remove('over');
+  }
+
+  function handleDrop(e) {
+    console.log("Drop");
+    if (e.stopPropagation) {
+      e.stopPropagation(); // stops the browser from redirecting.
+    }
+
+    if (dragSrcEl != this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData('text/html');
+    }
+
+    return false;
+  }
+
+  function handleDragEnd(e) {
+    console.log("DragEnd");
+    this.style.opacity = '1';
+
+    items.forEach(function (item) {
+      item.classList.remove('over');
+    });
+  }
+
+
+  let items = document.querySelectorAll('.container .box');
+  items.forEach(function(item) {
+    item.addEventListener('dragstart', handleDragStart, false);
+    item.addEventListener('dragenter', handleDragEnter, false);
+    item.addEventListener('dragover', handleDragOver, false);
+    item.addEventListener('dragleave', handleDragLeave, false);
+    item.addEventListener('drop', handleDrop, false);
+    item.addEventListener('dragend', handleDragEnd, false);
+  });
+});
