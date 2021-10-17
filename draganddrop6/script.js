fd252b0f81e2c3effa8846d94d9a6eb9ab174a1d
Arka Majhi <arka.majhi@gmail.com>
2021-10-17 16:30:55 +0530

    Create script.js

---
+++ draganddrop6/script.js
@@ -0,0 +1,46 @@
+let cards = document.querySelectorAll('.card');
+let containers = document.querySelectorAll('.container');
+
+console.log(cards);
+console.log(containers);
+
+cards.forEach(card => {
+  card.addEventListener('dragstart',function(){
+    console.log("Drag Started");
+    card.classList.add('dragging');
+  });
+  card.addEventListener('dragend',function(){
+    console.log("Drag Ended");
+    card.classList.remove('dragging');
+  });
+});
+
+containers.forEach(container => {
+  container.addEventListener('dragover',function(){
+    console.log("Hovered");
+    let current=document.querySelector('.dragging');
+    container.append(current);
+  });
+});
+
+function evenodd()
+{
+  let factor=Math.round(Math.random()*10);
+  if (factor%2===0) {return true}
+  else {return false}
+}
+
+window.addEventListener('DOMContentLoaded',function(){
+  let one=document.querySelector('.one');
+  let two=document.querySelector('.two');
+
+  cards.forEach(card => {
+    if (evenodd()) {
+      one.appendChild(card)
+    }
+    else {
+      two.appendChild(card)
+    }
+  });
+
+})
