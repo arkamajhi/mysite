41837953a39161aa2c8d68578e30c6cdee9b9b00
Arka Majhi <arka.majhi@gmail.com>
2021-10-29 02:53:21 +0530

    Update sw.js

--- immunization1/sw.js
+++ immunization1/sw.js
@@ -1,3 +1,8 @@
 self.addEventListener("install", e => {
   console.log("install");
+  e.waitUntil(
+    caches.open("static").then(cache => {
+      return cache.addAll(["./",".\css\dragAndDropStyle.css",".\images\logo192.png"])
+    })
+  );
 });
