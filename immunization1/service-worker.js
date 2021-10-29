225ab48160a2fac35f628bf4086b79139f2e304f
Arka Majhi <arka.majhi@gmail.com>
2021-10-29 13:14:50 +0530

    Update service-worker.js

--- immunization1/service-worker.js
+++ immunization1/service-worker.js
@@ -1,6 +1,10 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
-workbox.routing.registerRoute(
-  ({request})=>request.destination==='image',
-  new workbox.strategies.NetworkFirst()
-);
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.endsWith('.png')) {
+    // Oops! This causes workbox-strategies.js to be imported inside a fetch handler,
+    // outside of the initial, synchronous service worker execution.
+    const cacheFirst = new workbox.strategies.CacheFirst();
+    event.respondWith(cacheFirst.handle({request: event.request}));
+  }
+});
