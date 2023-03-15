d432427e06cd0e82c38d8fa27af4a6ede2e733c4
arkamajhi <arka.majhi@gmail.com>
2023-03-15 21:36:46 +0530

    Update fullscreen.js

--- dga/fullscreen.js
+++ dga/fullscreen.js
@@ -1,20 +1,20 @@
-var elem = document.documentElement;
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
+function toggleFullScreen() {
+    if (!document.fullscreenElement &&    // alternative standard method
+     !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      }
+    } else {
+       if (document.cancelFullScreen) {
+          document.cancelFullScreen();
+       } else if (document.mozCancelFullScreen) {
+          document.mozCancelFullScreen();
+       } else if (document.webkitCancelFullScreen) {
+         document.webkitCancelFullScreen();
+       }
+    }
   }
-}
-
-function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
-    document.msExitFullscreen();
-  }
-}
