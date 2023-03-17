520c227d31bc2e85e693bcbb6218db8e5d0cf191
arkamajhi <arka.majhi@gmail.com>
2023-03-18 03:48:02 +0530

    Update geojson.js

--- dga/geojson.js
+++ dga/geojson.js
@@ -8,5 +8,5 @@ for(var k in geoJson.features)
     state[k]=geoJson.features[k].properties.NAME2_;
     district[k]=geoJson.features[k].properties.NAME1_;
 }
-//console.log(state);
+console.log(state);
 
