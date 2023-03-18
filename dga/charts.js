9032bee3d9994d75511099adb12f8b6e2b6bc9b7
arkamajhi <arka.majhi@gmail.com>
2023-03-18 05:36:14 +0530

    Update charts.js

--- dga/charts.js
+++ dga/charts.js
@@ -1,3 +1,4 @@
+/*
 const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
@@ -18,3 +19,53 @@ const ctx = document.getElementById('myChart');
       }
     }
   });
+  */
+
+  /*
+
+  var ctx = document.getElementById('myChart');
+        var myLineChart = new Chart(ctx, {
+            type: 'radar',
+            data: {
+                labels: ['Running','Swimming', 'Eating','Cycling','Skating','Singing','Balling'],
+                datasets: [{
+                    data: [150, 164, 210, 195, 169, 184, 250],
+                    label: "A Team",
+                    fill: true,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    pointBackgroundColor: 'rgb(255, 99, 132)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgb(255, 99, 132)'
+                }, {
+                    data: [168, 170, 178, 190, 203, 276,279],
+                    label: "B Team",
+                    fill: true,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgb(54, 162, 235)',
+                    pointBackgroundColor: 'rgb(54, 162, 235)',
+                    pointBorderColor: '#fff',
+                    pointHoverBackgroundColor: '#fff',
+                    pointHoverBorderColor: 'rgb(54, 162, 235)'
+                }]
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: 'School Tournament(Max points - 300)'
+                }
+            }
+        });
+        */
+
+const charData = 'csv\dga_dh_2019.csv';
+d3.csv(charData).then(function(datapoints){
+    console.log(datapoints);
+    const cost=[];
+    for(i=0;i<datapoints.length;i++)
+    {
+        cost.push(datapoints.length[i.cost])
+    }
+    console.log(cost);
+});
