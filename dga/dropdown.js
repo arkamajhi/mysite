f8bc7e07d528eda1e526c4e647e388723af1604f
arkamajhi <arka.majhi@gmail.com>
2023-03-19 06:04:19 +0530

    Update dropdown.js

--- dga/dropdown.js
+++ dga/dropdown.js
@@ -1,7 +1,14 @@
+document.querySelectorAll('.phc-list').forEach(el => el.hidden = true);
+
 var uniquestate = (state.filter((value, index, array) => array.indexOf(value) === index)).sort();
 //console.log(uniquestate);
 
 let selectstate = document.getElementById('state');
+
+let opt = document.createElement("option");
+opt.innerHTML = "All States";
+selectstate.append(opt);
+
 uniquestate.map( (lang, i) => {
     let opt = document.createElement("option");
     opt.value = lang; // the index
@@ -10,8 +17,19 @@ uniquestate.map( (lang, i) => {
 });
 
 selectstate.addEventListener('change', display_district);
+
+let selectdistrict = document.getElementById('district');
+
 function display_district()
 {
+    document.querySelectorAll('.phc-list').forEach(el => el.hidden = true);
+
+    selectdistrict.innerHTML = "";
+    let opt = document.createElement("option");
+    //opt.value = i; // the index
+    opt.innerHTML = "All Districts";
+    selectdistrict.append(opt);
+
     console.log(selectstate.value);
 
 
@@ -27,13 +45,19 @@ function display_district()
 
     var uniquedistrict = (listdistict.filter((value, index, array) => array.indexOf(value) === index)).sort();
 
-    let selectdistrict = document.getElementById('district');
-    selectdistrict.innerHTML = "";
+
 
     uniquedistrict.map( (lang, i) => {
     let opt = document.createElement("option");
-    opt.value = i; // the index
+    opt.value = lang; // the index
     opt.innerHTML = lang;
     selectdistrict.append(opt);
     });
+}
+
+selectdistrict.addEventListener('change', changed_district);
+
+function changed_district()
+{
+    document.querySelectorAll('.phc-list').forEach(el => el.hidden = true);
 }
