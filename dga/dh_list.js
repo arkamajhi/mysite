299b540613723b1c12f3b18aee980c1340b01af0
arkamajhi <arka.majhi@gmail.com>
2023-03-19 06:04:21 +0530

    Create dh_list.js

---
+++ dga/dh_list.js
@@ -0,0 +1,488 @@
+var dhList = [
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.1927378,
+                20.71969445
+            ]
+        },
+        "properties": {
+            "name": "DH Balod",
+            "district": "Balod",
+            "state": "Chhattisgarh",
+            "address": "College Road, Balod, Chhattisgarh, 491226",
+            "phone": "7749223924",
+            "map": "https://goo.gl/maps/vVkqLSBMZDzhQACPA"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.14934355,
+                21.66345462
+            ]
+        },
+        "properties": {
+            "name": "DH Baloda Bazar",
+            "district": "Baloda Bazar",
+            "state": "Chhattisgarh",
+            "address": "Baloda Bazar, Chhattisgarh, 493332",
+            "phone": "",
+            "map": "https://goo.gl/maps/xZ6ezboSA6vBtV5E7"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                83.6190355,
+                23.61696467
+            ]
+        },
+        "properties": {
+            "name": "DH Balrampur",
+            "district": "Balrampur",
+            "state": "Chhattisgarh",
+            "address": "Balrampur, Chhattisgarh, 497119",
+            "phone": "",
+            "map": "https://goo.gl/maps/MCHgg3RaCaHgjRqK6"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.02393892,
+                19.08485231
+            ]
+        },
+        "properties": {
+            "name": "DH Bastar",
+            "district": "Bastar",
+            "state": "Chhattisgarh",
+            "address": "Naya Para, Chhattisgarh, 494001",
+            "phone": "",
+            "map": "https://goo.gl/maps/JQzBkBtchyWz9vCQ7"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.53371523,
+                21.7023915
+            ]
+        },
+        "properties": {
+            "name": "DH Bemetara",
+            "district": "Bemetara",
+            "state": "Chhattisgarh",
+            "address": "Bemetara, Chhattisgarh, 491335",
+            "phone": "",
+            "map": "https://goo.gl/maps/yDZAqiZWVwbxcKpB6"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                80.81225459,
+                18.79453869
+            ]
+        },
+        "properties": {
+            "name": "DH Bijapur",
+            "district": "Bijapur",
+            "state": "Chhattisgarh",
+            "address": "Dist.hospital chowk, main road, Bijapur, Chhattisgarh, 494444",
+            "phone": "",
+            "map": ""
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.15853097,
+                22.07427602
+            ]
+        },
+        "properties": {
+            "name": "DH Bilaspur",
+            "district": "Bilaspur",
+            "state": "Chhattisgarh",
+            "address": "Old Bus Stand, Chhattisgarh, 495004",
+            "phone": "8309977063",
+            "map": "https://goo.gl/maps/D8TjAee3fbWz1TAT6"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.35060194,
+                18.89340419
+            ]
+        },
+        "properties": {
+            "name": "DH Dantewada",
+            "district": "Dantewada",
+            "state": "Chhattisgarh",
+            "address": "Dantewada, Chhattisgarh, 494449",
+            "phone": "",
+            "map": "https://goo.gl/maps/2ehqWmHBMgTyVHwr9"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.53950892,
+                20.70789238
+            ]
+        },
+        "properties": {
+            "name": "DH Dhamtari",
+            "district": "Dhamtari",
+            "state": "Chhattisgarh",
+            "address": "Ratna Bhandha Rd, Subhash Nagar, Dhamtari, Chhattisgarh, 493773",
+            "phone": "",
+            "map": "https://goo.gl/maps/GPg4yngxUhHFbYgN7"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.27933211,
+                21.18744006
+            ]
+        },
+        "properties": {
+            "name": "DH Durg",
+            "district": "Durg",
+            "state": "Chhattisgarh",
+            "address": "Government District Hospital, Pachri Para, Chhattisgarh, 491001",
+            "phone": "",
+            "map": "https://goo.gl/maps/B8soSZiqgtyBLjN1A"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.05721506,
+                20.63672112
+            ]
+        },
+        "properties": {
+            "name": "DH Gariyaband",
+            "district": "Gariyaband",
+            "state": "Chhattisgarh",
+            "address": "DR. A R Menon Road, Gariyaband, Chhattisgarh, 493889",
+            "phone": "",
+            "map": ""
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.60720626,
+                22.00315333
+            ]
+        },
+        "properties": {
+            "name": "DH Janjgir Champa",
+            "district": "Janjgir Champa",
+            "state": "Chhattisgarh",
+            "address": "Collectorate Colony, Janjgir, Chhattisgarh, 495668",
+            "phone": "",
+            "map": "https://goo.gl/maps/Kni8M3fKS9rnVgL99"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                84.13725716,
+                22.88257665
+            ]
+        },
+        "properties": {
+            "name": "DH Jashpur",
+            "district": "Jashpur",
+            "state": "Chhattisgarh",
+            "address": "Purani Toli, Jashpur Nagar, Chhattisgarh, 496331",
+            "phone": "",
+            "map": "https://goo.gl/maps/AKY3k3GFHa9f14H87"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.21791828,
+                22.0087394
+            ]
+        },
+        "properties": {
+            "name": "DH Kabirdham",
+            "district": "Kabirdham",
+            "state": "Chhattisgarh",
+            "address": "Kawardha, Chhattisgarh, 491995",
+            "phone": "",
+            "map": "https://goo.gl/maps/BfdE9gcMGM8qpvrS9"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                83.39588848,
+                21.89269761
+            ]
+        },
+        "properties": {
+            "name": "DH Kanker",
+            "district": "Kanker",
+            "state": "Chhattisgarh",
+            "address": "NH 43, Govindpur, Kanker, Chhattisgarh, 494334",
+            "phone": "",
+            "map": "https://goo.gl/maps/qdU3hiMjkhABXm2D7"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.67454327,
+                19.56591012
+            ]
+        },
+        "properties": {
+            "name": "DH Kondagaon",
+            "district": "Kondagaon",
+            "state": "Chhattisgarh",
+            "address": "Kondagaon, Chhattisgarh, 494226",
+            "phone": "7587014600",
+            "map": "https://goo.gl/maps/nLthe7iWUcSqfyWp9"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.74398982,
+                22.36573058
+            ]
+        },
+        "properties": {
+            "name": "DH Korba",
+            "district": "Korba",
+            "state": "Chhattisgarh",
+            "address": "Rajgamar Rd, Kharmora, Industrial Area, Chhattisgarh, 495677",
+            "phone": "",
+            "map": "https://goo.gl/maps/Wxh9QWRVDmw6w9mWA"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.56339334,
+                23.2600957
+            ]
+        },
+        "properties": {
+            "name": "DH Korea",
+            "district": "Korea",
+            "state": "Chhattisgarh",
+            "address": "Jampara, Baikunthpur, Chhattisgarh, 497335",
+            "phone": "",
+            "map": "https://goo.gl/maps/tvmQufjpSahU4TYB6"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.07380911,
+                21.12007146
+            ]
+        },
+        "properties": {
+            "name": "DH Mahasamund",
+            "district": "Mahasamund",
+            "state": "Chhattisgarh",
+            "address": "Kharora, Chhattisgarh, 493445",
+            "phone": "",
+            "map": "https://goo.gl/maps/5yYxpd8GBynnaZ8o7"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.69199845,
+                22.06199524
+            ]
+        },
+        "properties": {
+            "name": "DH Mungeli",
+            "district": "Mungeli",
+            "state": "Chhattisgarh",
+            "address": "Mungeli, Chhattisgarh, 495334",
+            "phone": "",
+            "map": "https://goo.gl/maps/ewAz3jB34VnpWZf39"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.23373211,
+                19.72375608
+            ]
+        },
+        "properties": {
+            "name": "DH Narayanpur",
+            "district": "Narayanpur",
+            "state": "Chhattisgarh",
+            "address": "Narayanpur, Chhattisgarh, 494661",
+            "phone": "7389928740",
+            "map": "https://goo.gl/maps/nMvfmrSxDPYgfbdK8"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                83.39590537,
+                21.89277831
+            ]
+        },
+        "properties": {
+            "name": "DH Raigarh",
+            "district": "Raigarh",
+            "state": "Chhattisgarh",
+            "address": "Nayaganj, Chhattisgarh, 496001",
+            "phone": "",
+            "map": "https://goo.gl/maps/5nKqGYH4PxjsQazDA"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.64013018,
+                21.23183478
+            ]
+        },
+        "properties": {
+            "name": "DH Raipur",
+            "district": "Raipur",
+            "state": "Chhattisgarh",
+            "address": "Janta Colony, Byron Bazar, Janta Colony, Raipur,Chhattisgarh, 492001",
+            "phone": "",
+            "map": "https://goo.gl/maps/S6htdQ4cYHgUNtvXA"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.0302233,
+                21.0862153
+            ]
+        },
+        "properties": {
+            "name": "DH Rajnandgaon",
+            "district": "Rajnandgaon",
+            "state": "Chhattisgarh",
+            "address": "Dongargaon Rd, Basantpur, Rajnandgaon, Chhattisgarh, 491441",
+            "phone": "",
+            "map": "https://goo.gl/maps/53Kx6vpGYS4JSEMY9"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                81.65724766,
+                18.39307282
+            ]
+        },
+        "properties": {
+            "name": "DH Sukma",
+            "district": "Sukma",
+            "state": "Chhattisgarh",
+            "address": "District Hospital Sukma, Sukma, Chhattisgarh, 494111",
+            "phone": "",
+            "map": "https://goo.gl/maps/nNqVWZXS9vGn3jka8"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                82.88034238,
+                23.21003366
+            ]
+        },
+        "properties": {
+            "name": "DH Surajpur",
+            "district": "Surajpur",
+            "state": "Chhattisgarh",
+            "address": "NH 43, Surajpur, Chhattisgarh, 497229",
+            "phone": "",
+            "map": "https://goo.gl/maps/jXkBjMa9TuaHZY4e9"
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                83.19331791,
+                23.10998138
+            ]
+        },
+        "properties": {
+            "name": "DH Surguja",
+            "district": "Surguja",
+            "state": "Chhattisgarh",
+            "address": "Ambikapur, Chhattisgarh, 497001",
+            "phone": "",
+            "map": "https://goo.gl/maps/Wc4e7HkivDHRPM3w7"
+        }
+    }
+]
