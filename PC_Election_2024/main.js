34aa4b9890ce4015a7a0c3ab1db3db6625015b42
arkamajhi <arka.majhi@gmail.com>
2024-05-31 07:46:54 +0530

    Update main.js

--- PC_Election_2024/main.js
+++ PC_Election_2024/main.js
@@ -11,3 +11,13 @@ document.getElementById('btnSwitchdark').addEventListener('click',()=>{
         document.getElementById('btnSwitchdark').style="background: black";
     }
 })
+
+const csv= 'Andaman & Nicobar Islands,Andhra Pradesh,Arunachal Pradesh,Assam,Bihar,Chhattisgarh,Dadra & Nagar Haveli and Daman & Diu,Goa,Gujarat,Jammu and Kashmir,Jharkhand,Karnataka,Kerala,Ladakh,Lakshadweep,Madhya Pradesh,Maharashtra,Manipur,Meghalaya,Mizoram,Nagaland,Odisha,Puducherry,Rajasthan,Sikkim,Tamil Nadu,Telangana,Tripura,Uttar Pradesh,Uttarakhand,West Bengal';
+
+const rows = csv.split(',');
+
+console.log(rows);
+
+for (i = 0; i < rows.length; i ++){
+        document.getElementById('State_Results_Menu').innerHTML += '<li><a class="dropdown-item" href="#'+rows[i]+'">'+rows[i]+'</a></li><li><hr class="dropdown-divider"></li>';
+    }
